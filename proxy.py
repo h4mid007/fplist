d2813a926cafb0214ebec424b81139e880155843
Hamidreza <hamid.online.behbahan@gmail.com>
2020-04-24 21:38:29 +0430

    Pacs?

--- proxy.py
+++ proxy.py
@@ -39,20 +39,39 @@ for i,proxy in enumerate(data):
     except:
         pass
     sleep(3)
+    
+## txt proxies formatting
 prox = ''
 for proxy in proxies:
     prox = prox + proxy + '\n'
+## pac proxies formatting
+pacs = '''
+function FindProxyForURL(url, host) {
+return "{0}";
+}
+'''
+pac = ''
+for proxy in proxies:
+    pac = pac + 'PROXY '+ proxy + ';'
+pacs = pacs.format(pac)
 proxies = b64encode(str(prox).encode())
 jsons = b64encode(str(dumps(jsons)).encode())
+pacs = b64encode(str(pacs).encode())
+#Setting headers
 headers = {"Content-Type" : "application/vnd.github.v3+json", "Authorization" : "token " + token}
+#getting sha and updating txt
 sha = get(url='https://api.github.com/repos/h4mid007/free-proxy-list/contents/proxies.txt', headers=headers, verify=False).json()['sha']
 data = {"message": datetime.now().strftime("%D - %H:%M"), "content": proxies.decode(), "sha": sha}
 a = put(url='https://api.github.com/repos/h4mid007/free-proxy-list/contents/proxies.txt', json=data, headers=headers, verify=False)
-print(a.text)
+#getting sha and updating json
 sha = get(url='https://api.github.com/repos/h4mid007/free-proxy-list/contents/proxies.json', headers=headers, verify=False).json()['sha']
 data = {"message": datetime.now().strftime("%D - %H:%M"), "content": jsons.decode(), "sha": sha}
 a = put(url='https://api.github.com/repos/h4mid007/free-proxy-list/contents/proxies.json', json=data, headers=headers, verify=False)
-print(a.text)
+#getting sha and updating pac
+sha = get(url='https://api.github.com/repos/h4mid007/free-proxy-list/contents/proxies.pac', headers=headers, verify=False).json()['sha']
+data = {"message": datetime.now().strftime("%D - %H:%M"), "content": pacs.decode(), "sha": sha}
+a = put(url='https://api.github.com/repos/h4mid007/free-proxy-list/contents/proxies.pac', json=data, headers=headers, verify=False)
+
 
 
 
