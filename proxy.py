a896396317195a1a4b77e06c86ac2c5bbab818a9
Hamidreza <hamid.online.behbahan@gmail.com>
2020-04-30 19:00:17 +0430

    pac enhancement

--- proxy.py
+++ proxy.py
@@ -47,7 +47,17 @@ for proxy in proxies:
 ## pac proxies formatting
 pacs = '''
 function FindProxyForURL(url, host) {{
-return "{0}";
+    //Bypass local
+    if (isPlainHostName(host) ||
+        shExpMatch(host, "*.local") ||
+        isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+        isInNet(dnsResolve(host), "172.16.0.0",  "255.240.0.0") ||
+        isInNet(dnsResolve(host), "192.168.0.0",  "255.255.0.0") ||
+        isInNet(dnsResolve(host), "127.0.0.0", "255.255.255.0"))
+        return "DIRECT";
+        
+    //else
+        return "{0}";
 }}
 '''
 pac = ''
